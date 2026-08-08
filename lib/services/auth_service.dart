6df59d9140a5e90c58ba7f1cb213fdc7e9e5bd87
김성일 <kimsungil322@gmail.com>
2026-08-08 21:44:08 +0900

    Remove Apple Sign-In
    Apple 로그인 기능을 완전히 제거합니다. 로그인 화면의 Apple 버튼, AuthService의
    signInWithApple(), sign_in_with_apple 패키지 의존성, iOS Sign In with Apple
    entitlement를 삭제했습니다.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 const _redirectUrl = 'com.icisland.icislandapp://login-callback/';
@@ -28,30 +25,6 @@ class AuthService {
       OAuthProvider.kakao,
       redirectTo: _redirectUrl,
       authScreenLaunchMode: LaunchMode.externalApplication,
-    );
-  }
-
-  static Future<void> signInWithApple() async {
-    final rawNonce = _client.auth.generateRawNonce();
-    final hashedNonce = sha256.convert(utf8.encode(rawNonce)).toString();
-
-    final credential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-      nonce: hashedNonce,
-    );
-
-    final idToken = credential.identityToken;
-    if (idToken == null) {
-      throw const AuthException('Apple 로그인에 실패했어요 (identityToken 없음)');
-    }
-
-    await _client.auth.signInWithIdToken(
-      provider: OAuthProvider.apple,
-      idToken: idToken,
-      nonce: rawNonce,
     );
   }
 
